8b2353ac446a968d277d717ad2393891efa3762b
NorakiRebird <Rayann_Chaghla@outlook.fr>
2024-12-31 12:32:31 +0100

    Initial Commit

---
+++ AyoubTest/AyoubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AyoubTestApp.swift
+//  AyoubTest
+//
+//  Created by Rayann chaghla on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AyoubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
